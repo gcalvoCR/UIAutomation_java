da116d57db50dcd4518d918a61d9b6ace1fcf3d2
Gabriel Calvo Vargas <gabrielcalvovargas@Gabriels-MacBook-Pro.local>
2019-09-26 15:39:36 -0600

    changes according to explanation

--- src/test/java/testing/cap1/AppTest.java
+++ src/test/java/testing/cap1/AppTest.java
@@ -4,11 +4,21 @@ package testing.cap1;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AppTest 
+public class AppTest
 {
     @Test
-    public void firstTest()
-    {
-        Assert.assertTrue( true );
+    public void miPrimerTest(){
+
+        String mivariable = "Elemento logOut";
+        System.out.println(mivariable);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+        }
+
+        Assert.assertEquals(mivariable, "Accept button", "El elemento no fue encontrado");
+
     }
+
 }
