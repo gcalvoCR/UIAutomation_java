4e087ade15d04741954ffb4fb9c288a0a0d855a6
GABOCALVO\Gabriel <gabriel.calvo.vargas@gmail.com>
2019-09-26 11:28:45 -0600

    little change

--- src/test/java/testing/cap4/AppTest.java
+++ src/test/java/testing/cap4/AppTest.java
@@ -20,7 +20,7 @@ public class AppTest
     * */
     @BeforeMethod
     public void setUp(){
-        String path = "C:\\Users\\Gabriel\\Documents\\dev\\UIAutomation_java\\src\\test\\resources\\chromedriver.exe";
+        String path = "./src/test/resources/chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", path);
         driver = new ChromeDriver();
     }
